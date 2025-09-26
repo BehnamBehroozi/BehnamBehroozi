3fda12aa4e3ead88c215fbd0dc6e031b575fc0e5
Behnam Behroozi <behnambehroozi09@gmail.com>
2025-09-26 19:57:57 +0330

    Add files via upload

---
+++ TEST2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace TEST_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            Console.Write("add aval ra vard konid=");
+            Console.WriteLine(a);
+            Console.Write("add dovom ra vard konid=");
+            Console.WriteLine(b);
+            Console.Write("add sevom ra vard konid=");
+            Console.WriteLine(c);
+            Console.Write("bozorg tarin adad=");
+            {
+                if (c > b && c>a)
+                {
+                    Console.WriteLine(c);
+                }
+                Console.ReadKey();
+
+                
+            }
+        } 
+    }
+}
